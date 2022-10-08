b9702b330f6fade46cce28a94615f9a288c21d96
vikramabid <vikramabid012@gmail.com>
2022-10-08 12:02:25 +0530

    Initial commit

---
+++ apps/authentication/forms.py
@@ -0,0 +1,75 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from datetime import datetime
+from django.core.validators import RegexValidator
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = forms.CharField(validators=[phone_regex], max_length=17)
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+    date_of_birth = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "placeholder": "Date of birth",
+                "class": "form-control"
+            }
+        )
+
+    )
+
+
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'phone', 'date_of_birth', 'password1', 'password2')
